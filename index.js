59080460833c7dd31d936a9c84b3b91677774e41
JhoonHe <jjhonherrera113@gmail.com>
2023-01-14 14:05:27 -0500

    conexion mysql

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+let mysql = require('mysql');
+
 const app = express();
 const port = 10101;
 
@@ -8,4 +11,26 @@ app.get('/', (req, res) => {
 
 app.listen(port, () =>{
     console.log(`Conexión establecida en el puerto: ${port}`);
+})
+
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : 'localhost',
+    user            : 'root',
+    password        : '1234',
+    database        : 'modulos',
+    debugger        : false
+})
+
+app.get('/test', (req, res) => {
+    pool.query('select * from usuario', function (error, results, fields){
+        if (error) throw error
+        let correo          = results[0].correo;
+        let nombre          = results[0].nombre;
+        let apellido        = results[0].apellido;
+        let edad            = results[0].edad;
+        let telefono        = results[0].telefono;
+
+        res.send(`Datos del usuario: ${correo}, ${nombre}, ${apellido}, ${edad}, ${telefono}`)
+    })
 })
