535a50057f6d6dbc6dececa40fa80ed4727641e8
JhoonHe <jjhonherrera113@gmail.com>
2023-01-14 16:50:47 -0500

    Barra-nav

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-
+const express = require('express'); 
+const bodyParser = require('body-parser');
 let mysql = require('mysql');
 
 const app = express();
+
 const port = 10101;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use('/public/', express.static('public'));
+
+
 app.get('/', (req, res) => {
-    res.send('Conexión establecida correctamente')
+    // res.json('Conexión establecida correctamente')
+    res.render('index')
 })
 
 app.listen(port, () =>{
